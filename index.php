d8b7bf66899b121c2bebd1a93d3d64621961801a
George Ulahannan <george@xminds.com>
2020-12-14 18:33:48 +0530

    test

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello Xminds xmindsmxmindasdasdasdasda</p>'; ?> 
+ <?php echo '<p>Hello Xminds info tech test page</p>'; ?> 
  </body>
 </html>
